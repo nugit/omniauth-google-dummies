e41181fd5d88ce8388393008c8799b310c56f986
Alan Ho <alanho@gmail.com>
2013-10-12 19:07:13 +0800

    update

--- lib/omniauth/strategies/google_analytics.rb
+++ lib/omniauth/strategies/google_analytics.rb
@@ -3,6 +3,7 @@ require 'omniauth/strategies/google_oauth2'
 module OmniAuth
   module Strategies
     class GoogleAnalytics < OmniAuth::Strategies::GoogleOauth2
+      option :name, 'google_analytics'
     end
   end
 end
