da5df76c02b10b3633d73c810e5008c706ad038c
Alan Ho <alanho@gmail.com>
2014-09-05 01:08:01 +0800

    require the file

--- lib/omniauth/google_dummies.rb
+++ lib/omniauth/google_dummies.rb
@@ -1,3 +1,4 @@
 require File.join('omniauth', 'strategies', 'google_analytics')
 require File.join('omniauth', 'strategies', 'google_adwords')
 require File.join('omniauth', 'strategies', 'double_click')
+require File.join('omniauth', 'strategies', 'double_click_bid_manager')
